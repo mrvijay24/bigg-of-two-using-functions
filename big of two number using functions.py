37ae04ef8efb1f85f52d4d0f371305a7616491c2
mrvijay24 <110984700+mrvijay24@users.noreply.github.com>
2022-12-02 19:04:13 +0530

    Add files via upload

---
+++ big of two number using functions.py
@@ -0,0 +1,9 @@
+def biggoftwo(n1,n2):
+    if n1>n2:
+        print('biggest number:',n1)
+    else:
+        print('biggest number:',n2)
+print('enter the two number:')
+n1=int(input())
+n2=int(input())
+biggoftwo(n1,n2)
